67ab9d1a087680a0fe2de73110e78eef5dd854d8
Надя Титова <dyuuuhhh@gmail.com>
2021-06-13 19:33:44 +0300

    add findFriend and addFriend, fix load and save

--- Chat/Server/Data.c
+++ Chat/Server/Data.c
@@ -4,10 +4,12 @@
 #include "ServerParams.h"
 
 #define width 30		//width of logins and passwords
+#define width_fr 100    //width of friends
 #define length 100		//max amount of logins and passwords
 
 extern char logins[100][30];
 extern char passwords[100][30];
+extern char friends[100][100];
 
 int findPassword(int step, char pass[])
 {
@@ -23,9 +25,10 @@ void addUser(char name[], char pass[])
 	}
 	strcpy(logins[step], name);
 	strcpy(passwords[step], pass);
+	strcpy(friends[step], "\n");
 }
 
-int findLogin(char *name)
+int findLogin(char* name)
 {
 	int step = 0;
 	while (logins[step][0]) {
@@ -36,10 +39,66 @@ int findLogin(char *name)
 	return -1;
 }
 
+int findFriend(char* name, char* buf)
+{
+	char fr_name[30] = { 0 };
+	int n = 0;
+	for (int i = 5; i < strlen(buf); i++)
+	{
+		fr_name[n] = buf[i];
+		n++;
+	}
+	n = findLogin(name);
+	char temp_fr_name[30] = { 0 };
+	int t = 0;
+	for (int i = 0; i < width_fr; i++)
+	{
+		if (friends[n][i] != ' ')
+		{
+			temp_fr_name[t] = friends[n][i];
+			t++;
+		}
+		else
+		{
+			if (strcmp(temp_fr_name, fr_name) == 0)
+				return 0;
+			else
+			{
+				for (t = 0; t < width; t++)
+					temp_fr_name[t] = '\0';
+				t = 0;
+			}
+		}
+	}
+	int f = 0;
+	f = findLogin(fr_name);
+	strcpy(temp_fr_name, fr_name);
+	if (f > -1)
+	{
+		return 1;
+	}
+	return -1;
+}
+
+void addFriend(char* buf, char* userName)
+{
+	char fr_name[30] = { 0 };
+	int n = 0;
+	for (int i = 5; i < strlen(buf); i++)
+	{
+		fr_name[n] = buf[i];
+		n++;
+	}
+	n = findLogin(userName);
+	strcat(friends[n], fr_name);
+	strcat(friends[n], " ");
+}
+
 load()
 {
 	FILE* l_logins = fopen("Data/logins.txt", "r+");
 	FILE* l_passwords = fopen("Data/passwords.txt", "r+");
+	FILE* l_friends = fopen("Data/friends.txt", "r+");
 
 	for (int i = 0; i < length; i++)
 	{
@@ -48,10 +107,15 @@ load()
 			logins[i][j] = '\0';
 			passwords[i][j] = '\0';
 		}
+		for (int j = 0; j < width_fr; j++)
+		{
+			friends[i][j] = '\0';
+		}
 	}
 
 	int step = 0, len = 0;
 	char tempStr[30] = { 0 };
+	char tempStr_fr[100] = { 0 };
 	while (!feof(l_logins))
 	{
 		fgets(tempStr, 30, l_logins);
@@ -65,26 +129,36 @@ load()
 		if (tempStr[len] == '\n')
 			tempStr[len] = '\0';
 		strcpy(passwords[step], tempStr);
+
+		fgets(tempStr_fr, 100, l_friends);
+		len = (strlen(tempStr_fr) - 1);
+		if (tempStr_fr[len] == '\n')
+			tempStr_fr[len] = '\0';
+		strcpy(friends[step], tempStr_fr);
 		step++;
 	}
 
 	fclose(l_logins);
 	fclose(l_passwords);
+	fclose(l_friends);
 }
 
 save()
 {
 	FILE* l_logins = fopen("Data/logins.txt", "r+");
 	FILE* l_passwords = fopen("Data/passwords.txt", "r+");
+	FILE* l_friends = fopen("Data/friends.txt", "r+");
 
 	int step = 0;
 	while (logins[step][0])
 	{
 		fprintf(l_logins, "%s\n", logins[step]);
 		fprintf(l_passwords, "%s\n", passwords[step]);
+		fprintf(l_friends, "%s\n", friends[step]);
 		step++;
 	}
 
 	fclose(l_logins);
 	fclose(l_passwords);
+	fclose(l_friends);
 }
